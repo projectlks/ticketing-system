e095b25ec4d59fc7690319b8c9fe60d3f48ab081
LinKar <mglinkar08@gmail.com>
2025-08-28 17:30:24 +0630

    v-1.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default function Page({ params }: { params: { locale: string } }) {
-  redirect(`lang/${params.locale}/main/dashboard`);
+export default function Page() {
+  redirect(`lang/en/main/dashboard`);
 }
